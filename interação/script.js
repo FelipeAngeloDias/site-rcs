5607f25dab6334d808678e3131bf01c58f1ef33d
Felipe <felipeangelodias@live.com>
2022-08-10 17:59:09 -0300

    atualização dia 10.08.22

--- interação/script.js
+++ interação/script.js
@@ -16,16 +16,23 @@ function prev(){
 setInterval(next, 4000);
 
 function inicializar() {
-    var coordenadas = {lat: -22.912869, lng: -43.228963};
+  var coordenadas1 = {lat: -22.854667543645387, lng: -42.332491402622004};
+  var coordenadas2 = {lat: -22.85415344222655, lng: -43.47835648727772};
+   
 
-    var mapa = new google.maps.Map(document.getElementById('mapa'), {
-      zoom: 15,
-      center: coordenadas 
-    });
+  var mapa = new google.maps.Map(document.getElementById('mapa'), {
+    zoom: 9.3,
+    center: coordenadas1 
+  });
 
-    var marker = new google.maps.Marker({
-      position: coordenadas,
-      map: mapa,
-      title: 'Meu marcador'
-    });
-  }
+  var marker = new google.maps.Marker({
+    position: coordenadas1,
+    map: mapa,
+    title: 'RCS SUBAQUÁTICA - MATRIZ'
+  });
+  var marker = new google.maps.Marker({
+    position: coordenadas2,
+    map: mapa,
+    title: 'RCS SUBAQUÁTICA - FILIAL'
+  });
+}
