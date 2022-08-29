0edcab3b4c849d476ac417e95a778b74daa6bc94
Felipe <felipeangelodias@live.com>
2022-08-29 16:16:14 -0300

    atualização do site 29.08.22 16:16

--- interação/script.js
+++ interação/script.js
@@ -1,3 +1,5 @@
+//Função da anamição das fotos//
+
 let slides =  document.querySelectorAll('.slide-container');
 let index = 0;
 
@@ -14,6 +16,8 @@ function prev(){
 }
 
 setInterval(next, 4000);
+
+//Função do google maps//
 
 function inicializar() {
   var coordenadas1 = {lat: -22.854667543645387, lng: -42.332491402622004};
@@ -35,4 +39,21 @@ function inicializar() {
     map: mapa,
     title: 'RCS SUBAQUÁTICA - FILIAL'
   });
+}
+
+//Função rolagem com menu//
+
+const header = document.querySelector("header");
+const logoImg = documentquerySelector("#cablogo");
+
+window.addEventListener("scroll", diminuirHeader);
+
+function diminuirHeader(){
+  if(window.pageYOffset > 100 && header.classList.contains("max")){
+    header.classList.remove("max");
+    header.classList.add("min");
+  } else if (window.pageYOffset == 0 && header.classList.contains("min")){
+    header.classList.add("max");
+    header.classList.remove("min");
+  }
 }
